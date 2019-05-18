4ba431021590692cb183b62541307064c9bb94fc
Karol-Wroblewski <Karol-Wroblewski@users.noreply.github.com>
2019-05-18 13:13:56 +0200

    Updates

---
+++ precache-manifest.4a7b69814c0a3513f761753b7083eeac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5707b5e7975e6b83aa6b43d17c4d181e",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "2a6e008c1559249cb645",
+    "url": "/Todo-App/static/css/main.77c14cfe.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Todo-App/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "2a6e008c1559249cb645",
+    "url": "/Todo-App/static/js/main.8b261af1.chunk.js"
+  },
+  {
+    "revision": "2acd21dc4a6582beab29",
+    "url": "/Todo-App/static/js/runtime~main.3fb21290.js"
+  }
+]);
